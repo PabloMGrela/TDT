4e10344407762cffb7b20182de389b12f784f669
Pablo_Grela <pmgrela@gmail.com>
2020-03-05 21:07:27 +0100

    Initial commit.

---
+++ Assets/Tuto/Scripts/BuildManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private GameObject turretToBuild;
+    void Awake(){
+        if(instance != null){
+            Debug.LogError("More than one BuildManager in scene");
+            return;
+        }
+        instance = this;
+    
+    }
+    public GameObject standardTurretPrefab;
+
+    void Start(){
+        turretToBuild = standardTurretPrefab;
+    }
+    public GameObject GetTurretToBuild(){
+        return turretToBuild;
+    }
+}
