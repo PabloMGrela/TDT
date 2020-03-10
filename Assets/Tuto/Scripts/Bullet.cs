f0f01c6326bc64ecfb7e19bf4c85eedf2868beb7
Pablo Martínez Grela <p.martinez.grela@mobgen.com>
2020-03-10 18:40:45 +0100

    Added explosions.

--- Assets/Tuto/Scripts/Bullet.cs
+++ Assets/Tuto/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour
     private Transform target;
     public float speed = 70f;
     public GameObject impactEffect;
+    public float explosionRadius = 0f;
     public void Seek(Transform _target){
         target = _target;
     }
@@ -31,12 +32,45 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     void HitTarget(){
         GameObject effectIns = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 2f);
+
+        if(explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
-        Destroy(target.gameObject);
+    }
+
+    void Damage (Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
